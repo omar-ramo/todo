db675cfe2436b228bbf713139f5a72e6d7f30741
omar-ramo <ramiritoramo@gmail.com>
2019-06-05 00:02:22 +0000

    Configure django to use gmail for mailing in production

--- todo/settings/prod.py
+++ todo/settings/prod.py
@@ -19,4 +19,10 @@ DATABASES = {
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
